92a3efeec399977a97d90fe2ebe3120fa573f4f3
IHac-er <rtharun2012@gmail.com>
2026-09-16 00:55:25 +0530

    Refactor TicTacToe class into a seperate file

--- main.py
+++ main.py
@@ -1,49 +1,5 @@
 from bot import TicTacToeBot
-
-class OccupiedPosition(Exception): 
-    pass 
-
-class TicTacToe: 
-    def __init__(self):
-        self.board = ['-','-','-','-','-','-','-','-','-']
-
-    def game_over(self) -> bool:
-        for row in range(0, 9, 3):
-            if self.board[row] != '-' and self.board[row] == self.board[row+1] == self.board[row+2]:
-                print(f"{self.board[row]} Wins!")
-                return True
-            
-        for col in range(3):
-            if self.board[col] != '-' and self.board[col] == self.board[col+3] == self.board[col+6]: 
-                print(f"{self.board[col]} Wins!")
-                return True 
-            
-        if self.board[0] != '-' and self.board[0] == self.board[4] == self.board[8]: 
-            print(f"{self.board[0]} Wins!")
-            return True 
-        
-        if self.board[2] != '-' and self.board[2] == self.board[4] == self.board[6]: 
-            print(f"{self.board[2]} Wins!")
-            return True 
-        
-        if all(cell != '-' for cell in self.board): 
-            print("Draw!")
-            return True 
-        
-        return False
-
-    def play_move(self, position, token) -> None:
-        if self.board[position]  != '-':
-            raise OccupiedPosition("Position is occupied!")
-        self.board[position] = token
-
-    def get_board(self) -> list[str]:
-        return self.board
-
-    def print_board(self) -> None:
-        for i in range(0,7,3):
-            print(f"{self.board[i]} {self.board[i+1]} {self.board[i+2]}")
-    
+from TicTacToe import TicTacToe, OccupiedPosition
 
 def main():
     print("=" * 100)
