88ff1dc6d9f1ffbc719de7692ab014d0a0de37c4
IHac-er <rtharun2012@gmail.com>
2026-09-15 11:48:05 +0530

    Add minimax logic to bot.py
    Add best_move function that will generate all get all the available moves and perform minimax of each move to get a score of either 0/1/-1 and return the move with the highest possible score, this probably concludes the bot logic

--- bot.py
+++ bot.py
@@ -38,4 +38,51 @@ class TicTacToeBot:
 
     def undo_move(self, position) -> None: 
         self.board[position] = '-'
-    
+
+    def minimax(self, max_player):
+        winner = self.is_game_over() 
+        if winner: 
+            if winner == self.bot_token:
+                return 1 
+            else: 
+                return -1 
+        
+        if self.is_draw():
+            return 0 
+        
+        if max_player:
+            max_score = float('-inf')
+
+            for position in self.available_moves():
+                self.play_move(position, self.bot_token)
+                max_score = max(max_score, self.minimax(False))
+                self.undo_move(position)
+
+            return max_score
+        
+        else: 
+            min_score = float('inf')
+
+            for position in self.available_moves():
+                self.play_move(position, self.player_token)
+                min_score = min(min_score, self.minimax(True))
+                self.undo_move(position)
+
+            return min_score
+
+    def best_move(self):
+        best_score = float('-inf')
+        best_position = None
+
+        for position in self.available_moves():
+            self.play_move(position, self.bot_token)
+
+            current_score = self.minimax(False)
+
+            self.undo_move(position)
+
+            if current_score > best_score: 
+                best_score = current_score
+                best_position = position
+
+        return best_position
