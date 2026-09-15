6d38976ad2209a135df59b7f5bbcf2e905cf7167
IHac-er <rtharun2012@gmail.com>
2026-09-15 11:43:03 +0530

    Initialize TicTacToeBot class with necessary parameter in bot.py

---
+++ bot.py
@@ -0,0 +1,7 @@
+class TicTacToeBot: 
+    def __init__(self, bot_token, player_token):
+        self.board = ['-','-','-','-','-','-','-','-','-']
+        self.bot_token = bot_token
+        self.player_token = player_token
+
+    
